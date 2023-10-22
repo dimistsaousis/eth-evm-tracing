9b759aca2bfbb40cc52ddc8ceaefeadabea75e90
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-22 21:58:48 +0300

    new rust project

--- alchemy.py
+++
@@ -1,4 +0,0 @@
-from api_keys import ALCHEMY_API_KEY
-
-ALCHEMY_HTTP_URL = f"https://eth-mainnet.g.alchemy.com/v2/{ALCHEMY_API_KEY}"
-ALCHEMY_WS_URL = f"wss://eth-mainnet.g.alchemy.com/v2/{ALCHEMY_API_KEY}"
--- api_keys.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-ALCHEMY_API_KEY = os.getenv("ALCHEMY_API_KEY")
-ETHERSCAN_API_KEY = os.getenv("ETHERSCAN_API_KEY")

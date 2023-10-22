2a3ed79de265ea8787a0e70f02ab242bf8b1956d
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-22 18:29:25 +0300

    error handling for abi from etherscan

--- contract_abi.py
+++ contract_abi.py
@@ -6,10 +6,14 @@ from api_keys import ETHERSCAN_API_KEY
 def get_abi_from_etherscan(contract_address):
     url = f"https://api.etherscan.io/api?module=contract&action=getabi&address={contract_address}&apikey={ETHERSCAN_API_KEY}"
     response = requests.get(url)
-    if response.status_code == 200:
-        result = response.json()
-        if result["status"] == "1":
-            abi = json.loads(result["result"])
-            return abi
-        print(f"Error: {result['message']}")
-    print(f"HTTP error: {response.status_code}")
+
+    if response.status_code != 200:
+        raise requests.HTTPError(response=response)
+
+    result = response.json()
+
+    if result["status"] != "1":
+        raise requests.HTTPError(response=response)
+
+    abi = json.loads(result["result"])
+    return abi
